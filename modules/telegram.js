1b50f6bedc2f90194fa57d3c859032291f99eeea
hjf <hjf@hjf.com.ar>
2021-01-07 09:12:01 -0300

    telegram module no longer compresses

--- modules/telegram.js
+++ modules/telegram.js
@@ -1,8 +1,5 @@
 const logger = require('../logger')
 const db = require('../db')
-const imagemin = require("imagemin");
-const imageminMozjpeg = require("imagemin-mozjpeg");
-const pngToJpeg = require('png-to-jpeg');
 
 const TelegramBot = require('node-telegram-bot-api');
 
@@ -10,16 +7,10 @@ const tconfig = db.getSetting('telegram');
 const bot = new TelegramBot(tconfig.access_token, { polling: true });
 
 async function postImage(filepath, caption) {
-  let buf = await resizeImage(filepath)
-  buf = buf[0].data
-  return bot.sendPhoto(tconfig.chat_ids[0], buf, { caption: caption }, { filename: `${Date.now()}.jpg`, contentType: "image/jpeg" });
+  logger.info(`Posting image ${filepath} to telegram chat id ${tconfig.chat_ids[0]}`)
+  return bot.sendPhoto(tconfig.chat_ids[0], filepath, { caption: caption }, { filename: `${Date.now()}.jpg`, contentType: "image/jpeg" });
 }
 
 module.exports = {
   postImage: postImage
-}
-
-async function resizeImage(filepath, quality = 80) {
-  let buf = await imagemin([filepath], { destination: '.', plugins: [pngToJpeg({ quality: 100 }), imageminMozjpeg({ quality: quality })] });
-  return buf;
 }
