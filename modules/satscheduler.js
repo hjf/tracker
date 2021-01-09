62daa4860225be953e78e22931fe5de35ae7ffdf
hjf <hjf@hjf.com.ar>
2021-01-09 10:28:40 -0300

    predictions rad to deg

--- modules/satscheduler.js
+++ modules/satscheduler.js
@@ -79,6 +79,8 @@ async function generateSchedule(force) {
 
       for (let prediction of predictions) {
         prediction.sun_position = SunCalc.getPosition(prediction.start, location.lat, location.lon)
+        prediction.sun_position.azimuth = prediction.sun_position.azimuth * 180 / Math.PI;
+        prediction.sun_position.elevation = prediction.sun_position.elevation * 180 / Math.PI;
       }
 
       logger.debug(`${predictions.length} passes found.`)
@@ -94,7 +96,7 @@ async function generateSchedule(force) {
 
     }
     return true
-    
+
   } catch (err) {
     logger.error(err.message)
     return false
