8c8002bf2c69befc9de59299d088bcfa462bc2ee
hjf <hjf@hjf.com.ar>
2021-04-17 20:38:56 -0300

    messages have uuid

--- db.js
+++ db.js
@@ -2,6 +2,12 @@ const sqlite3 = require('sqlite3')
 const { open } = require('sqlite')
 const fs = require('fs')
 const path = require('path')
+const logger = require('./logger')
+const DATABASE_VERSION = 1
+
+const MIGRATION_SCRIPTS = {
+  1: ['delete from logs', 'alter table logs add uuid string']
+}
 
 function getConnection () {
   return new Promise((resolve, reject) => {
@@ -12,6 +18,48 @@ function getConnection () {
       .then((db) => { resolve(db) })
       .catch((err) => { reject(err) })
   })
+}
+
+async function doMigrations () {
+  logger.info('Checking migrations')
+  try {
+    const db = await getConnection()
+
+    const checkVersionTableExists = await db.all("select name from sqlite_master where name='db_version';")
+    if (checkVersionTableExists.length === 0) {
+      logger.info('version table does not exist, creating')
+      await db.run('create table db_version (version number)')
+      await db.run('insert into db_version (version) values (0);')
+    } else {
+      logger.info('version table exists')
+    }
+
+    let currentVersion = (await db.all('select version from db_version'))[0].version
+    logger.info(`current database version: ${currentVersion}`)
+
+    while (currentVersion < DATABASE_VERSION) {
+      currentVersion++
+      logger.info(`Migrating to version ${currentVersion}`)
+      await db.run('begin transaction;')
+      try {
+        for (const script of MIGRATION_SCRIPTS[currentVersion]) {
+          logger.info(script)
+          await db.run(script)
+        }
+        db.run(`update db_version set version = ${currentVersion};`)
+        db.run('commit transaction;')
+        logger.info(`Migrated to ${currentVersion}`)
+      } catch (err) {
+        await db.run('rollback;')
+        logger.error('Migration failed')
+        return false
+      }
+    }
+  } catch (err) {
+    logger.error(err)
+    return false
+  }
+  return true
 }
 
 // returns the peding scheduled events
@@ -82,13 +130,22 @@ function getSetting (key) {
 
 async function log (info) {
   const db = await getConnection()
-
-  return await db.run('INSERT INTO logs (timestamp, level, service, message) VALUES (:timestamp , :level , :service , :message)',
+  if (DATABASE_VERSION < 1) {
+    return await db.run('INSERT INTO logs (timestamp, level, service, message) VALUES (:timestamp , :level , :service , :message  )',
+      {
+        ':timestamp': Date.now(),
+        ':level': info.level,
+        ':service': info.service,
+        ':message': info.message
+      })
+  }
+  return await db.run('INSERT INTO logs (timestamp, level, service, message, uuid) VALUES (:timestamp , :level , :service , :message , :uuid )',
     {
       ':timestamp': Date.now(),
       ':level': info.level,
       ':service': info.service,
-      ':message': info.message
+      ':message': info.message,
+      ':uuid': info.uuid
     })
 }
 
@@ -107,5 +164,6 @@ module.exports = {
   deleteScheduledEvents: deleteScheduledEvents,
   getScheduledEventsToRun: getScheduledEventsToRun,
   log: log,
-  getLogs: getLogs
+  getLogs: getLogs,
+  doMigrations: doMigrations
 }
